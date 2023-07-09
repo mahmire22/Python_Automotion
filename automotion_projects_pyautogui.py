4e9458d25a43e6ccea6ccdeab7cb3668ce55aa8c
Maruf Ahmed <41677456+mahmire22@users.noreply.github.com>
2023-07-09 17:52:46 +0100

    Add files via upload

---
+++ automotion_projects_pyautogui.py
@@ -0,0 +1,16 @@
+import pyautogui
+import  time
+
+#Give the python file some time before continuing
+time.sleep(3)
+
+#Mouse Function
+print(pyautogui.size()) #prints the resolution of your screen
+print(pyautogui.position()) #prints the current position
+pyautogui.moveTo(100, 100, 3)#moves the mouse over time (3 seconds)
+pyautogui.moveRel(100, 100, 3)#move the mouse relative to its current position
+pyautogui.click(100, 100, 3, 3, button="left")
+
+
+pyautogui.scroll(500) #scroll up
+pyautogui.scroll(-500) #scroll down
